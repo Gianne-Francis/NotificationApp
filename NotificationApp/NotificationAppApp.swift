b78f2d4a796cfa0720cf74bb5ea3cc77b2c42973
Gianne-Francis <tinier.caddy.0g@icloud.com>
2023-07-24 16:45:00 -0400

    Initial Commit

---
+++ NotificationApp/NotificationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationAppApp.swift
+//  NotificationApp
+//
+//  Created by Gianne Francis on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct NotificationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
